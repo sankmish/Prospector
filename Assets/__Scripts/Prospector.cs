c5cd6f41b71a48df7c6a550e9a021277c71ee21d
sankmish <mishra.sancho@gmail.com>
2018-07-12 16:22:13 -0400

    Prospector Putting Cards into SlotDefs
    Created Layout Script to take Prospector and parse cards into slotdefs.

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -11,10 +11,12 @@ public class Prospector : MonoBehaviour {
 
 	[Header("Set in Inspector")]
 	public TextAsset			deckXML;
+    public TextAsset layoutXML;
 
 
 	[Header("Set Dynamically")]
 	public Deck					deck;
+    public Layout layout;
 
 	void Awake(){
 		S = this;
@@ -23,6 +25,10 @@ public class Prospector : MonoBehaviour {
 	void Start() {
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
+        Deck.Shuffle(ref deck.cards);
+
+        layout = GetComponent<Layout>();
+        layout.ReadLayout(layoutXML.text);
 	}
 
 }
