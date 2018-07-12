14aaca9a4379370f664ee2f271b6f3d317c9428a
SPM170 <SPM170@CILABS-114B-13.scilsnet.rutgers.edu>
2018-07-12 18:37:15 -0400

    Prospector Adding Scoring
    Prospector now keeps track of score, scorRun, highscore, and score from prev round.

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -183,6 +183,7 @@ public class Prospector : MonoBehaviour {
                 MoveToDiscard(target);
                 MoveToTarget(Draw());
                 UpdateDrawPile();
+				ScoreManager.EVENT (eScoreEvent.draw);
                 break;
 
             case eCardState.tableau:
@@ -199,6 +200,7 @@ public class Prospector : MonoBehaviour {
                 tableau.Remove(cd);
                 MoveToTarget(cd);
                 SetTablueaFaces();
+				ScoreManager.EVENT (eScoreEvent.mine);
                 break;
         }
 
@@ -234,10 +236,12 @@ public class Prospector : MonoBehaviour {
         if (won)
         {
             print("Game Over. You won! :) ");
+			ScoreManager.EVENT (eScoreEvent.gameWin);
         }
         else
         {
             print("Game Over. You Lost :( ");
+			ScoreManager.EVENT (eScoreEvent.gameLoss);
         }
 
         SceneManager.LoadScene("__Prospector_Scene_0");
